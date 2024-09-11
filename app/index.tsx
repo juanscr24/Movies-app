401dcb1b24693880afd56be410b7e9c6e42d1ed7
juanscr24 <juanscr24@hotmail.com>
2024-09-11 12:14:34 -0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View >
+      <View>
+        <Text>Hello World</Text>
+        <Text>This is the first page of your app.</Text>
+      </View>
+    </View>
+  );
+}
